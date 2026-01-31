8bb2da452da16655d1ffbaeb2e957943a7d15fac
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-01 00:21:58 +0100

    feat(theme): provide typography based on the fonts

--- composeApp/src/commonMain/kotlin/com/example/themingkmp/theme/Typography.kt
+++ composeApp/src/commonMain/kotlin/com/example/themingkmp/theme/Typography.kt
@@ -1,8 +1,11 @@
 package com.example.themingkmp.theme
 
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.Font
 import themingkmp.composeapp.generated.resources.Res
 import themingkmp.composeapp.generated.resources.inter_medium
@@ -26,5 +29,39 @@ val Inter
         Font(
             resource = Res.font.inter_medium,
             weight = FontWeight.Medium
+        ),
+    )
+
+val Typography: Typography
+    @Composable get() = Typography(
+        bodyLarge = TextStyle(
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal,
+            fontSize = 17.sp,
+            lineHeight = 24.sp
+        ),
+        bodyMedium = TextStyle(
+            fontFamily = Inter,
+            fontWeight = FontWeight.Medium,
+            fontSize = 15.sp,
+            lineHeight = 20.sp
+        ),
+        bodySmall = TextStyle(
+            fontFamily = Inter,
+            fontWeight = FontWeight.Normal,
+            fontSize = 15.sp,
+            lineHeight = 20.sp
+        ),
+        titleLarge = TextStyle(
+            fontFamily = SpaceGrotesk,
+            fontWeight = FontWeight.Bold,
+            fontSize = 32.sp,
+            lineHeight = 36.sp
+        ),
+        titleSmall = TextStyle(
+            fontFamily = SpaceGrotesk,
+            fontWeight = FontWeight.Medium,
+            fontSize = 17.sp,
+            lineHeight = 24.sp
         ),
     )
