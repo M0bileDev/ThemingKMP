9a479ce8f479092ab821b780668bb8f0e0d00d0b
m0bileDev <damian.ogorek.1996@gmail.com>
2026-02-05 23:11:44 +0100

    feat(utils): provide constant value for tablet and desktop

--- composeApp/src/commonMain/kotlin/com/example/themingkmp/utils/AdaptiveBreakpoints.kt
+++ composeApp/src/commonMain/kotlin/com/example/themingkmp/utils/AdaptiveBreakpoints.kt
@@ -1,5 +1,7 @@
 package com.example.themingkmp.utils
 
+const val TABLET_DESKTOP_MAX_WIDTH = 540
+
 enum class AdaptiveBreakpoints(val width: Int, val height: Int) {
     MEDIUM(600, 480),
     EXPANDED(840, 900)
